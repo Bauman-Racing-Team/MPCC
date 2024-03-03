644a3745200aaf0dde114435cc8d9bd9c402d6bb
Jinlong Gongye <j_gongye@baumanracing.ru>
2024-03-03 14:48:57 +0300

    [MPCC-42] (+) add tire ellipse constraints on front and rear wheels

--- Matlab/plot/@Plot/raceAngles.m
+++ Matlab/plot/@Plot/raceAngles.m
@@ -110,6 +110,25 @@ function raceAngles(obj)
     title(rearSlipAngle,'rearSlipAngle');
     ylabel(rearSlipAngle,'rearSlipAngle');
 
+    % Track error
+    x = states(1,:);
+    y = states(2,:);
+    xTrack = zeros(1,length(obj.log));
+    yTrack = zeros(1,length(obj.log));
+    for i = 1:length(obj.log)
+        xTrack(i) = obj.log(i).circlesCenters(1,1);
+        yTrack(i) = obj.log(i).circlesCenters(2,1);
+    end
+    trackError = sqrt((x-xTrack).^2 + (y-yTrack).^2);
+    figure;
+    plot(1:length(obj.log),trackError);
+
+    yline(-obj.parameters.mpcModel.maxDistProj,'--red','minDistProj'); % lower bound
+    yline(obj.parameters.mpcModel.maxDistProj,'--red','maxDistProj'); % upper bound
+    yline(-obj.parameters.mpcModel.rOut,'--red','minROut'); % lower bound
+    yline(obj.parameters.mpcModel.rOut,'--red','maxROut'); % upper bound
+    axis ([0 length(obj.log) -obj.parameters.mpcModel.maxDistProj-0.5 obj.parameters.mpcModel.maxDistProj+0.5]);
+
     % Ellipse
 
     Frx = zeros(length(obj.log),1);
@@ -117,57 +136,16 @@ function raceAngles(obj)
     Ffx = zeros(length(obj.log),1);
     Ffy = zeros(length(obj.log),1);
     for i = 1:length(obj.log)
-            vx = states(4,i);
-            vy = states(5,i);
-            r = states(6,i);
-            throttle = states(8,i);
-            steeringAngle = states(9,i);
-            brakes = states(10,i);
-            lr = obj.parameters.car.lr;
-            rearSlipAngles(i) = atan2(vy-r*lr,vx);
-
-    m = obj.parameters.car.m;
-    gAcc = obj.parameters.car.g;
-    fzNominal = obj.parameters.car.fzNominal;
-    cbr=obj.parameters.car.cbr;
-    cbf=obj.parameters.car.cbf;
-    rDyn = obj.parameters.car.rDyn;
-    cdrv = obj.parameters.car.cm1 * obj.parameters.car.gearRatio;
-    
-    Frz = lf*m*gAcc/(2.0*(lf+lr));
-    Drfz = (Frz-fzNominal)/fzNominal;
-    Kry = obj.parameters.tire.PKY1*fzNominal * sin(2.0*atan2(Frz,(obj.parameters.tire.PKY2*fzNominal*obj.parameters.tire.LFZO)))*obj.parameters.tire.LFZO*obj.parameters.tire.LKY;
-    Dry = (obj.parameters.tire.PDY1+obj.parameters.tire.PDY2*Drfz)*obj.parameters.tire.LMUY*Frz;
-    Cry = obj.parameters.tire.PCY1*obj.parameters.tire.LCY;
-    Bry = Kry/(Cry*Dry);
-    Ery = (obj.parameters.tire.PEY1+obj.parameters.tire.PEY2*Drfz)*obj.parameters.tire.LEY;
-    sar = atan2((vy-r*lr),vx);
-    
-    Frx(i) = (-cbr*brakes)/rDyn*tanh(vx)+(cdrv*throttle)/rDyn+2*obj.parameters.tire.QSY1*Frz*tanh(vx);
-%     Fry(i) = 2*Dry*sin(Cry*atan(Bry*sar-Ery*(Bry*sar-atan(Bry*sar))));
-    Fry(i) = -sar*29117;
-
-    Ffz = lr*m*gAcc/(2.0*(lf+lr));
-    Dffz = (Ffz-fzNominal)/fzNominal;
-    Kfy = obj.parameters.tire.PKY1*fzNominal * sin(2.0*atan2(Ffz,(obj.parameters.tire.PKY2*fzNominal*obj.parameters.tire.LFZO)))*obj.parameters.tire.LFZO*obj.parameters.tire.LKY;        
-    mufy = (obj.parameters.tire.PDY1+obj.parameters.tire.PDY2*Dffz)*obj.parameters.tire.LMUY;
-    Dfy = mufy*Ffz;
-    Cfy = obj.parameters.tire.PCY1*obj.parameters.tire.LCY;
-    Bfy = Kfy/(Cfy*Dfy);
-    Efy = (obj.parameters.tire.PEY1+obj.parameters.tire.PEY2*Dffz)*obj.parameters.tire.LEY;
-    saf = atan2((vy+r*lf),vx)-steeringAngle;
-    
-%     Ffy(i) = 2*Dfy*sin(Cfy*atan(Bfy*saf-Efy*(Bfy*saf-atan(Bfy*saf))));
-    Ffx(i) = (-cbf*brakes)/rDyn*tanh(vx)+2*obj.parameters.tire.QSY1*Ffz*tanh(vx);
-  
-    Ffy(i) = -saf*29117;
-
+        state = states(:,i);
+        [Ffx(i),Ffy(i),Frx(i),Fry(i)] = obj.carModel.initSimpleFrictionEllipseConstraint(state);
     end
-    
+
     figure;
+    subplot(1,2,1);
     hold on;
-    equation = @(x, y) (x/2118.04).^2 + (y/2030.5).^2 - 1;
-    ezplot(equation, [-2118.04, 2118.04, -2030.5, 2030.5]);
+    axis equal;
+    equation = @(x, y) (x/obj.parameters.car.muxFz).^2 + (y/obj.parameters.car.muyFz).^2 - 1;
+    ezplot(equation, [-obj.parameters.car.muxFz, obj.parameters.car.muxFz, -obj.parameters.car.muyFz, obj.parameters.car.muyFz]);
     h = get(gca, 'Children');
     set(h, 'Color', 'r','LineStyle','--');
     plot(Ffx,Ffy,'.')
@@ -175,21 +153,21 @@ function raceAngles(obj)
     xlabel('Ffx');
     ylabel('Ffy');
 
-    figure;
+    subplot(1,2,2);
     hold on;
-    equation = @(x, y) (x/2118.04).^2 + (y/2030.5).^2 - 1;
-    fimplicit(equation, [-2118.04, 2118.04, -2030.5, 2030.5]);
+    axis equal;
+    equation = @(x, y) (x/obj.parameters.car.muxFz).^2 + (y/obj.parameters.car.muyFz).^2 - 1;
+    ezplot(equation, [-obj.parameters.car.muxFz, obj.parameters.car.muxFz, -obj.parameters.car.muyFz, obj.parameters.car.muyFz]);
     h = get(gca, 'Children');
     set(h, 'Color', 'r','LineStyle','--');
     plot(Frx,Fry,'.')
-
-%     plot(rearSlipAngle,1:length(obj.log),rearSlipAngles);
-
-%     ylim padded;
 
     title('Tire force of rear force');
     xlabel('Frx');
     ylabel('Fry');
 
+
 end
+
+
 
