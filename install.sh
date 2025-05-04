192abb908c2841dd752fa997d4a100e2e4338255
AnastasiiaMalaia <anastasiia.malaia@nami.ru>
2025-05-05 00:19:33 +0300

    [MPCC-49] (+) updated casadi version, fixed acados commit

--- install.sh
+++ install.sh
@@ -9,16 +9,16 @@ git clone https://github.com/lava/matplotlib-cpp.git
 git clone https://gitlab.com/libeigen/eigen.git
 
 cd acados
+git checkout 6673a07ad
 git submodule update --recursive --init
 mkdir build
-cd build && cmake ..
+cd build && cmake .. -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 make -j$(nproc) && make install
 cd ../external
-wget -q -nc --show-progress https://github.com/casadi/casadi/releases/download/3.6.6/casadi-linux-matlabR2014b-v3.6.6.tar.gz
+wget -q -nc --show-progress https://github.com/casadi/casadi/releases/download/3.6.6/casadi-3.6.6-linux64-matlab2018b.zip
 mkdir -p casadi-matlab
-tar -xf casadi-linux-matlabR2014b-v3.6.6.tar.gz -C casadi-matlab
+unzip casadi-3.6.6-linux64-matlab2018b.zip -d casadi-matlab
 cd ../../
-
 
 ACADOS_LIB_PATH=${ACADOS_LIB_PATH:-"$(pwd)/acados/lib"}
 echo "$ACADOS_LIB_PATH" | sudo tee /etc/ld.so.conf.d/acados.conf
